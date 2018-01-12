2ecb52eac5a49ff128e0740ff56652cc083f82be
bipinkh <bipinkh91@gmail.com>
2018-01-12 23:19:41 +0545

    signature check. sha3 for multiple arguments left to do

--- src/main/java/contracts/raw/setget.sol
+++ src/main/java/contracts/raw/setget.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.0;
 contract SetGetBC {
     uint storedData;
     address notary;
+    uint result = 0;
     
     function set(uint a)public{
         storedData = a;
@@ -19,16 +20,24 @@ contract SetGetBC {
      function getNotaryAddress() public constant returns(address not){
        return notary;  
     }
-    
+
+    function getResult() public constant returns(uint not){
+           return result;
+        }
+
     function checkHash(string msgString, uint256 msgUint, address msgAddrs, uint8 v, bytes32 r, bytes32 s) public
-        constant returns (bool success){
+         returns (bool success){
 
         bytes32 msgHash = keccak256(msgString, msgUint, msgAddrs);
 
         address signerAddrs = ecrecover(msgHash, v, r, s);
 
-        if (notary != signerAddrs) return false;
+        if (notary != signerAddrs){
+        result = 2;
+        return false;
+        }
 
+        result = 1;
         return true;
 
     }

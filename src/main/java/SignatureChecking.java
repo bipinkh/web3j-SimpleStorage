1e4e56fff835312cf3a00a9caedcbc4cb9471cc2
bipinkh <bipinkh91@gmail.com>
2018-01-12 23:29:13 +0545

    signature checking and verification

--- src/main/java/SignatureChecking.java
+++ src/main/java/SignatureChecking.java
@@ -1,6 +1,9 @@
 import contracts.NodeConstants;
 import contracts.wrapper.SetGetBC;
 import contracts.wrapper.SimpleStorage;
+import org.bouncycastle.crypto.digests.KeccakDigest;
+import org.bouncycastle.jcajce.provider.digest.Keccak;
+import org.bouncycastle.jcajce.provider.digest.SHA3;
 import org.web3j.crypto.ECKeyPair;
 import services.SignatureService;
 import contracts.loader;
@@ -14,8 +17,8 @@ public class SignatureChecking {
 
     public static void main(String[] args){
         contract  = loader.getSetGetContract();
-        setNotary();
-
+//        setNotary();
+        checkSign();
     }
 
     private static void setNotary() {
@@ -33,13 +36,35 @@ public class SignatureChecking {
                 });
     }
 
+
+    //todo this
     public static void checkSign(){
 
         //prepare signature
         ECKeyPair kp = ECKeyPair.create(new BigInteger(NodeConstants.PRIVATE_KEY_NOTARY, 16));
-        String msgString = new String("hello world");
-        BigInteger _balance = BigInteger.valueOf(100000); //1 thousand
-        SignatureService.SignatureData signature = SignatureService.signMessage(msgString.getBytes(), kp);
+
+        String msgString = new String("bipinkhatiwada");
+        BigInteger msgInt = BigInteger.valueOf(3);
+        String msgAddress = "0xf17f52151EbEF6C7334FAD080c5704D77216b732";
+
+        String input = msgString + String.valueOf(msgInt) + msgAddress;
+        SHA3.Digest256 digestSHA3 = new SHA3.Digest256();
+        byte[] hashbytes = digestSHA3.digest(input.getBytes());
+
+        SignatureService.SignatureData signature = SignatureService.signMessage(hashbytes, kp);
+
+        contract.checkHash(msgString, msgInt, msgAddress,
+                BigInteger.valueOf(signature.getV()), signature.getR(), signature.getS())
+                .observable()
+                .subscribe(x -> {
+                    System.out.println("Tx hash :  " + x.getTransactionHash());
+                    try{
+                        BigInteger value = contract.getResult().send();
+                        System.out.println("Result value stored :: "+ value);
+                    }catch (Exception e){
+                        System.out.println("could not read result from contract");
+                    }
+                });
 
 
     }
